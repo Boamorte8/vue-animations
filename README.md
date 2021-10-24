0f409ec1b4f088cd9636d8d0eab59665215fad12
Esteban Salazar Boamorte <salazar.z.esteban@gmail.com>
2021-10-23 21:40:34 -0500

    Animating with javascript and vue

--- README.md
+++ README.md
@@ -4,4 +4,10 @@ This project is to learn about animations on Vue
 
 ## Learned things
 
-- 1
+- Vue has the transition tag to help us to animate some elements
+- You can rename the default classes to something that you want
+- transition doesn't work through components if this has multiples children. The component must has only 1 child
+- There is 1 exception when there is a v-if with a v-else
+- transition tag has some hooks very useful (beforeEnter, afterEnter, beforeLeave, afterLeave, enter, leave)
+- Control animations full with javascript with the hooks added
+- Exist the property :css to optimize the animations telling to vue that doesn't search animations on css because the animations is fully controlled by javascript
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,120 @@
 <template>
   <div class="container">
-    <div class="block"></div>
-    <button>Animate</button>
+    <div class="block" :class="{ animate: animatedBlock }"></div>
+    <button @click="animateBlock">Animate</button>
   </div>
-  <base-modal @close="hideDialog" v-if="dialogIsVisible">
+  <div class="container">
+    <!-- <transition enter-to-class="some-class" enter-active-class="some-class..."> -->
+    <transition name="para" appear
+                :css="false"
+                @before-enter="beforeEnter"
+                @before-leave="animationEvent($event, 'beforeLeave')"
+                @enter="enter"
+                @leave="leave"
+                @leave-cancelle="leaveCancelled"
+                @enter-cancelled="enterCancelled"
+                @after-leave="animationEvent($event, 'afterLeave')"
+                @after-enter="animationEvent($event, 'afterEnter')">
+                <!-- @before-enter="animationEvent($event, 'beforeEnter')"
+                @before-leave="animationEvent($event, 'beforeLeave')"
+                @enter="animationEvent($event, 'enter')"
+                @leave="animationEvent($event, 'leave')"
+                @after-leave="animationEvent($event, 'afterLeave')"
+                @after-enter="animationEvent($event, 'afterEnter')"> -->
+      <p v-if="paraIsVisible">This is only sometimes visible...</p>
+    </transition>
+    <button @click="toggleParagraph">Toggle Paragraph</button>
+  </div>
+  <div class="container">
+    <transition name="fade-button" mode="out-in">
+      <button @click="showUsers" v-if="!usersAreVisible">Show Users</button>
+      <button @click="hideUsers" v-else>Hide Users</button>
+    </transition>
+  </div>
+  <base-modal @close="hideDialog" :open="dialogIsVisible">
     <p>This is a test dialog!</p>
     <button @click="hideDialog">Close it!</button>
   </base-modal>
   <div class="container">
     <button @click="showDialog">Show Dialog</button>
   </div>
-</template>  
+</template>
 
 <script>
 export default {
   data() {
-    return { dialogIsVisible: false };
+    return {
+      animatedBlock: false,
+      dialogIsVisible: false,
+      paraIsVisible: false,
+      usersAreVisible: false,
+      enterInterval: null,
+      leaveInterval: null,
+    };
   },
   methods: {
+    animateBlock() {
+      this.animatedBlock = true;
+    },
+    toggleParagraph() {
+      this.paraIsVisible = !this.paraIsVisible;
+    },
     showDialog() {
       this.dialogIsVisible = true;
     },
     hideDialog() {
       this.dialogIsVisible = false;
+    },
+    showUsers() {
+      this.usersAreVisible = true;
+    },
+    hideUsers() {
+      this.usersAreVisible = false;
+    },
+    animationEvent(el, event) {
+      console.log(el, event);
+    },
+    beforeEnter(el) {
+      console.log('beforeEnter');
+      console.log(el);
+      el.style.opacity = 0;
+    },
+    leave(el, done) {
+      console.log('leave');
+      console.log(el);
+      let round = 1;
+      this.leaveInterval = setInterval(() => {
+        el.style.opacity = 1 - round * 0.01;
+        round++;
+        if (round > 100) {
+          clearInterval(this.leaveInterval);
+          done();
+        }
+      }, 20);
+    },
+    enter(el, done) {
+      console.log('enter');
+      console.log(el);
+      let round = 1;
+      this.enterInterval = setInterval(() => {
+        el.style.opacity = round * 0.03;
+        round++;
+        if (round > 100) {
+          clearInterval(this.enterInterval);
+          done();
+        }
+      }, 20);
+    },
+    enterCancelled(el) {
+      console.log('enterCancelled');
+      console.log(el);
+      clearInterval(this.enterInterval);
+      // done();
+    },
+    leaveCancelled(el) {
+      console.log('leaveCancelled');
+      console.log(el);
+      clearInterval(this.leaveInterval);
     },
   },
 };
@@ -57,6 +149,7 @@ button:active {
   height: 8rem;
   background-color: #290033;
   margin-bottom: 2rem;
+  /* transition: transform 1s ease-out; */
 }
 .container {
   max-width: 40rem;
@@ -68,5 +161,85 @@ button:active {
   padding: 2rem;
   border: 2px solid #ccc;
   border-radius: 12px;
+}
+
+.animate {
+  /* transform: translateX(-150px); */
+  animation: slide-fade .8s ease-out forwards;
+}
+
+/* .v-enter-from {
+  opacity: 0;
+  transform: translateY(-30px);
+}
+
+.v-enter-active {
+  transition: all .3s ease-out;
+}
+
+.v-enter-to {
+  opacity: 1;
+  transform: translateY(0);
+} */
+/* .v-enter-from {
+  opacity: 0;
+  transform: translateY(-30px);
+} */
+
+/* .para-enter-active {
+  animation: slide-scale .5s ease-out reverse;
+} */
+
+/* .v-leave-from {
+  opacity: 1;
+  transform: translateY(0);
+}
+
+.v-leave-active {
+  transition: all .3s ease-out;
+}
+
+.v-leave-to {
+  opacity: 0;
+  transform: translateY(-30px);
+} */
+
+/* .para-leave-active {
+  animation: slide-scale .5s ease-out;
+} */
+
+/* ================================================ */
+/* ==================Button Users Animation======== */
+
+.fade-button-enter-from,
+.fade-button-leave-to {
+  opacity: 0;
+}
+
+.fade-button-enter-active {
+  transition: opacity .4s ease-out;
+}
+
+.fade-button-leave-active {
+  transition: opacity .4s ease-in;
+}
+
+.fade-button-enter-to,
+.fade-button-leave-from {
+  opacity: 1;
+}
+
+@keyframes slide-scale {
+  0% {
+    transform: translateX(0) scale(1);
+  }
+
+  70% {
+    transform: translateX(-120px) scale(1.1);
+  }
+
+  100% {
+    transform: translateX(-150px) scale(1);
+  }
 }
 </style>
